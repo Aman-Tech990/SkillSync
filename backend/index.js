63e8cb41eda1ef4887c0fa66ce612761ac003bf7
Aman Parida <amantechz467@gmail.com>
2025-07-24 13:27:24 +0530

    Setting up for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
+
+const _dirname = path.resolve();
 
 if (process.env.NODE_ENV != `production`) {
     dotenv.config({});
@@ -25,6 +28,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
+
 // Deafult Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,6 +39,11 @@ app.use(`/api/v1/user`, userRoute);
 app.use(`/api/v1/company`, companyRoute);
 app.use(`/api/v1/job`, jobRoute);
 app.use(`/api/v1/application`, applicationRoute);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get((_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
